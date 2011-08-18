7a4b6149fad8030281773f5bdc55e235db061947
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-18 13:39:18 +0200

    Session storage calls normalization

--- lib/blocks/BlockNewBaseeventAction.class.php
+++ lib/blocks/BlockNewBaseeventAction.class.php
@@ -46,8 +46,7 @@ class event_BlockNewBaseeventAction extends website_BlockAction
 		// Check registered user restriction.
 		if (!$configuration->getAllowNotRegistered() && users_UserService::getInstance()->getCurrentFrontEndUser() === null)
 		{
-			$agaviUser = change_Controller::getInstance()->getContext()->getUser();
-			$agaviUser->setAttribute('illegalAccessPage', $_SERVER["REQUEST_URI"]);
+			change_Controller::getInstance()->getStorage()->writeForUser('users_illegalAccessPage', $_SERVER["REQUEST_URI"]);
 			return 'Forbidden';
 		}
 				
