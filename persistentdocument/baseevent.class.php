93f28b5c3aa55e2f36534a30199b380b0a715ddd
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-08-24 11:56:19 +0200

    [NEW] Indexation refactoring

--- persistentdocument/baseevent.class.php
+++ persistentdocument/baseevent.class.php
@@ -3,27 +3,9 @@
  * Class where to put your custom methods for document event_persistentdocument_baseevent
  * @package modules.event.persistentdocument
  */
-class event_persistentdocument_baseevent extends event_persistentdocument_baseeventbase implements indexer_IndexableDocument, rss_Item
+class event_persistentdocument_baseevent extends event_persistentdocument_baseeventbase implements rss_Item
 {
-	/**
-	 * Get the indexable document
-	 *
-	 * @return indexer_IndexedDocument
-	 */
-	public function getIndexedDocument()
-	{
-		$indexedDoc = new indexer_IndexedDocument();
-		$indexedDoc->setId($this->getId());
-		$indexedDoc->setDocumentModel($this->getPersistentModel()->getName());
-		$indexedDoc->setLabel($this->getLabel());
-		$indexedDoc->setLang(RequestContext::getInstance()->getLang());
-		$indexedDoc->setText(
-			f_util_StringUtils::htmlToText($this->getSummary())
-			. ' ' . f_util_StringUtils::htmlToText($this->getText())
-		);
-		return $indexedDoc;
-	}
-	
+
 	/**
 	 * @return string
 	 */
