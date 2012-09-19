9610b8711bc87b41f0b807e6416531fb56d5ddb0
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:39:54 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -8,16 +8,4 @@ class event_Setup extends object_InitDataSetup
 	{
 		$this->executeModuleScript('init.xml');
 	}
-
-	/**
-	 * @return string[]
-	 */
-	public function getRequiredPackages()
-	{
-		// Return an array of packages name if the data you are inserting in
-		// this file depend on the data of other packages.
-		// Example:
-		// return array('modules_website', 'modules_users');
-		return array();
-	}
 }
