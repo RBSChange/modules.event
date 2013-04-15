af53f05ca8de11f09c579eb968474b383e50867d
Gaël PORT <gael.port@rbs.fr>
2013-04-15 14:29:25 +0200

    [FIX] Update bean population.

--- lib/blocks/BlockNewEventAction.class.php
+++ lib/blocks/BlockNewEventAction.class.php
@@ -6,6 +6,19 @@
 class event_BlockNewEventAction extends event_BlockAbstractNewBaseeventAction
 {
 	/**
+	 * @return string[]|null
+	 */
+	public function getEventBeanInclude()
+	{
+		if (Framework::getConfigurationValue('modules/website/useBeanPopulateStrictMode') != 'false')
+		{
+			return array('label', 'date', 'endDate', 'detailvisual', 'summary', 'text', 'authorEmail', 'authorFirstName',
+				'authorLastName', 'authorWebsiteUrl');
+		}
+		return null;
+	}
+
+	/**
 	 * @param f_mvc_Request $request
 	 * @param f_mvc_Response $response
 	 * @param event_persistentdocument_event $event
@@ -15,7 +28,7 @@ class event_BlockNewEventAction extends event_BlockAbstractNewBaseeventAction
 	{
 		return parent::executeSubmit($request, $response, $event);
 	}
-	
+
 	/**
 	 * @param f_mvc_Request $request
 	 * @param f_mvc_Response $response
--- lib/blocks/BlockNewNewsAction.class.php
+++ lib/blocks/BlockNewNewsAction.class.php
@@ -6,6 +6,19 @@
 class event_BlockNewNewsAction extends event_BlockAbstractNewBaseeventAction
 {
 	/**
+	 * @return string[]|null
+	 */
+	public function getNewsBeanInclude()
+	{
+		if (Framework::getConfigurationValue('modules/website/useBeanPopulateStrictMode') != 'false')
+		{
+			return array('label', 'date', 'detailvisual', 'summary', 'text', 'authorEmail', 'authorFirstName',
+				'authorLastName', 'authorWebsiteUrl');
+		}
+		return null;
+	}
+
+	/**
 	 * @param f_mvc_Request $request
 	 * @param f_mvc_Response $response
 	 * @param event_persistentdocument_news $news
